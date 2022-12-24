9020f4ff759730bf4de46e981eca1544e01667a6
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-24 10:15:16 +0300

    navbar css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Home } from './component/Home';
-import './App.css';
+import React, { Compoent } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./component/Home";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-   <Home/>
-       
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
